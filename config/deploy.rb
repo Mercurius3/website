d978afd4d606145af819349d1e10af0552771474
Axel Graff <axelgraff@gmail.com>
2013-01-01 22:17:36 +0100

    fixed typo in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, roles: :app, except: { no_release: true } do
-    run "#{try_sudo} touch #{File.join(current_path, 'tmp','restrat.txt')}"
+    run "#{try_sudo} touch #{File.join(current_path, 'tmp','restart.txt')}"
   end
   
   after "deploy:update_code", :link_production_db
