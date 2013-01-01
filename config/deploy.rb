64e136783bdb566ec990accf2490a744049ed910
Axel Graff <axelgraff@gmail.com>
2013-01-01 17:52:25 +0100

    changed path in deploy.rb to absolute path

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,12 +15,9 @@ set :repository,  "git@github.com:Mercurius3/website.git"
 # Gebruik de standaard Ruby van de server
 set :rvm_ruby_string, 'default'
 
-# Fix de error 'no such file or directory'
-set :normalize_asset_timestamps, false
-
 # De onderstaande instellingen zijn specifiek voor de Bluerail servers, u
 # hoeft hier zelf geen wijzigingen in aan te brengen.
-set :deploy_to, "~/rails"
+set :deploy_to, "/var/www/vhosts/lassche-lassche.nl/rails"
 set :rvm_type, :system
 set :rvm_bin_path, '/usr/local/rvm/bin'
 set :use_sudo, false
