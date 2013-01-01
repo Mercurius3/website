5ca1f2bb35e3120fd38b5c82926ffef9397e846c
Axel Graff <axelgraff@gmail.com>
2013-01-01 20:09:21 +0100

    Added recipes for symlinking to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,20 @@ set :repository,  "git@github.com:Mercurius3/website.git"
 # Gebruik de standaard Ruby van de server
 set :rvm_ruby_string, 'default'
 
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, roles: :app, except: { no_release: true } do
+    run "#{try_sudo} touch #{File.join(current_path, 'tmp','restrat.txt')}"
+  end
+  
+  after "deploy:update_code", :link_production_db
+  before "deploy:assets:precompile" do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+    # run "ln -nfs #{deploy_to}/shared/config/application.yml #{release_path}/config/application.yml"
+    # run "ln -nfs #{deploy_to}/shared/config/newrelic.yml #{release_path}/config/newrelic.yml"
+end
+
 # De onderstaande instellingen zijn specifiek voor de Bluerail servers, u
 # hoeft hier zelf geen wijzigingen in aan te brengen.
 set :deploy_to, "/var/www/vhosts/lassche-lassche.nl/rails"
