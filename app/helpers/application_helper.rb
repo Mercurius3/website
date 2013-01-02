12f27d5ede8638a9378963aeaed0ff68137516f2
Axel Graff <axelgraff@gmail.com>
2013-01-02 10:42:50 +0100

    Refactored kramdown method to application helper as markdown method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def markdown(content)
+    raw Kramdown::Document.new(content, auto_ids: :false, parse_block_html: :true).to_html
+  end
+
   def switch_mobile_view
     content_tag :p, link_to(other_view, switch_mobile_view_path, "data-ajax" => false)
   end
