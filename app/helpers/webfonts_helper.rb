7c8eca7aee0a95614e994dbf62ee3e15742ad9b2
Axel Graff <axelgraff@gmail.com>
2013-01-16 18:53:25 +0100

    switched to hosting own fonts

--- app/helpers/webfonts_helper.rb
+++ app/helpers/webfonts_helper.rb
@@ -5,7 +5,7 @@ module WebfontsHelper
 
   def webfont_url
     if Rails.env.development?
-      "http://f.fontdeck.com/s/css/l4MsFun30OJhB9DTDGcb8JQSAOQ/localhost/2208.css"
+      "http://f.fontdeck.com/s/css/l4MsFun30OJhB9DTDGcb8JQSAOQ/website.dev/2208.css"
     elsif Rails.env.production?
       "http://f.fontdeck.com/s/css/l4MsFun30OJhB9DTDGcb8JQSAOQ/lassche-lassche.nl/2208.css"
     elsif Rails.env.staging?
