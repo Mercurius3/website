f3be08300f190d513ab4567276e4fe3a906dd966
Axel Graff <axelgraff@gmail.com>
2013-01-17 09:06:13 +0100

    Testing static assets in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Website::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -34,4 +34,5 @@ Website::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
 end
