5f3838c88719d2161b33073454d97282ff7632e0
Axel Graff <axelgraff@gmail.com>
2013-01-18 23:27:50 +0100

    Added stuff to .htaccess, including .woff gzip compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,5 +67,5 @@ Website::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.assets.precompile += %w( .woff)
+  config.assets.precompile += %w( .woff )
 end
