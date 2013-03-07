07763320a2643d9295d48d7057e89d78890e56c1
Axel Graff <axelgraff@gmail.com>
2013-03-07 10:42:06 +0100

    bundle update --deployment

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', :path => '~/Dropbox/Github/rails'
+gem 'rails'#, :path => '~/Dropbox/Github/rails'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
---
+++ vendor/bundle/ruby/1.9.1/gems/activeadmin-sortable-0.0.1/lib/activeadmin-sortable.rb
@@ -0,0 +1,36 @@
+require 'activeadmin-sortable/version'
+require 'activeadmin'
+require 'rails/engine'
+
+module ActiveAdmin
+  module Sortable
+    module ControllerActions
+      def sortable
+        member_action :sort, :method => :post do
+          resource.insert_at params[:position].to_i
+          head 200
+        end
+      end
+    end
+
+    module TableMethods
+      HANDLE = '&#x2195;'.html_safe
+
+      def sortable_handle_column
+        column '' do |resource|
+          sort_url = url_for([:sort, :admin, resource])
+          content_tag :span, HANDLE, :class => 'handle', 'data-sort-url' => sort_url
+        end
+      end
+    end
+
+    ::ActiveAdmin::ResourceDSL.send(:include, ControllerActions)
+    ::ActiveAdmin::Views::TableFor.send(:include, TableMethods)
+
+    class Engine < ::Rails::Engine
+      # Including an Engine tells Rails that this gem contains assets
+    end
+  end
+end
+
+
