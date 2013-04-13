ad721841d6046bc6a934c4c88a851657ce51105d
Axel Graff <axelgraff@gmail.com>
2013-04-13 22:03:22 +0200

    added position to attr_accessible

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ActiveRecord::Base
-  attr_accessible :content, :name, :permalink
+  attr_accessible :content, :name, :permalink, :position
   validates_uniqueness_of :permalink
 
   acts_as_list
