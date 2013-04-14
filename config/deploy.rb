ab2db2531eba9af5b269ec059e70bde9fd6e19bd
Axel Graff <axelgraff@gmail.com>
2013-04-14 16:28:54 +0200

    fixed deploy.rb, but still no multistage functionality

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,8 +18,10 @@ set :repository,  "git@github.com:Mercurius3/website.git"
 # De onderstaande instellingen zijn specifiek voor de Bluerail servers, u
 # hoeft hier zelf geen wijzigingen in aan te brengen.
 # set :deploy_to, lambda { capture("echo -n ~/rails") }
-set :deploy_to,"/var/www/vhosts/lassche-lassche.nl/rails"
-set :rvm_type, :system
+set :deploy_to, lambda { capture("echo -n ~/staging") }
+# set :rvm_type, :system
+set :rvm_path, '/usr/local/rvm'
+set :rvm_bin_path, '/usr/local/rvm/bin'
 set :use_sudo, false
 set :keep_releases, 1
 
@@ -34,12 +36,15 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-
-  after "deploy:update_code", :link_production_db
+  before "deploy:assets:precompile", :create_symlinks
+  after "deploy", "deploy:cleanup"
+  task :seed do
+    run "cd #{deploy_to}/current && bundle exec rake db:seed RAILS_ENV=staging && touch tmp/restart.txt"
+  end
 end
 
 desc "Link database.yml from shared path"
-task :link_production_db do
+task :create_symlinks do
   run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
   run "ln -nfs #{deploy_to}/shared/config/initializers/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
   run "ln -nfs #{deploy_to}/shared/config/application.yml #{release_path}/config/application.yml"
