9a38f2ca0b14a1c1ed24a5bdd027ad2c17c3c099
Axel Graff <axelgraff@gmail.com>
2013-04-16 16:57:31 +0200

    optimizing images

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -38,6 +38,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb do
     process :resize_to_limit => [200, 200]
+    process :quality => 95
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,3 +9,17 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
 end
+
+module CarrierWave
+  module RMagick
+
+    def quality(percentage)
+      manipulate! do |img|
+        img.write(current_path){ self.quality = percentage } unless img.quality == percentage
+        img = yield(img) if block_given?
+        img
+      end
+    end
+
+  end
+end
